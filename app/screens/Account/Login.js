42a3d1c723997feb3ed78005a500705f35ae59e7
Jose Antonio Garcia Perez <josse89@gmail.com>
2020-04-10 11:55:13 +0200

    Laptop

--- app/screens/Account/Login.js
+++ app/screens/Account/Login.js
@@ -1,10 +1,21 @@
 import React from "react";
 import { StyleSheet, View, ScrollView, Text, Image } from "react-native";
+import { Divider } from "react-native-elements";
 
 export default function Login() {
   return (
-    <View>
-      <Text>Login Form...</Text>
-    </View>
+    <Image
+      source={require("../../../assets/img/5-tenedores-letras-icono-logo.png")}
+      style={styles.logo}
+      resizeMode="contain"
+    />
   );
 }
+
+const styles = StyleSheet.create({
+  logo: {
+    width: "100%",
+    height: 150,
+    marginTop: 20,
+  },
+});
