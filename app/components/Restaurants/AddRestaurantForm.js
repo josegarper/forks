07cff1cd93ac65d34118d329d49b803f14a43866
josegarper <josse89@gmail.com>
2020-04-14 18:03:32 +0200

    78

--- app/components/Restaurants/AddRestaurantForm.js
+++ app/components/Restaurants/AddRestaurantForm.js
@@ -3,19 +3,61 @@ import { StyleSheet, View, ScrollView, Alert, Dimensions } from "react-native";
 import { Icon, Avatar, Image, Input, Button } from "react-native-elements";
 import * as Permissions from "expo-permissions";
 import * as ImagePicker from "expo-image-picker";
+import MapView from "react-native-map";
+import Modal from "../Modal";
+
+const widthScreen = Dimensions.get("window").width;
 
 export default function AddRestaurantForm(props) {
   const { toastRef, setIsLoading, navigation } = props;
   const [imagesSelected, setImagesSelected] = useState([]);
+  const [restaurantName, setRestaurantName] = useState("");
+  const [restaurantAddress, setRestaurantAddress] = useState("");
+  const [restaurantDescription, setRestaurantDescription] = useState("");
+  const [isVisibleMap, setIsVisibleMap] = useState(false);
+  const [locationRestaurant, setLocationRestaurant] = useState(null);
 
   return (
     <ScrollView>
+      <ImageRestaurant imageRestaurant={imagesSelected[0]} />
+      <FormAdd
+        setRestaurantName={setRestaurantName}
+        setRestaurantAddress={setRestaurantAddress}
+        setRestaurantDescription={setRestaurantDescription}
+        setIsVisibleMap={setIsVisibleMap}
+        locationRestaurant={locationRestaurant}
+      />
       <UploadImage
         imagesSelected={imagesSelected}
         setImagesSelected={setImagesSelected}
         toastRef={toastRef}
       />
+      <Map
+        isVisibleMap={isVisibleMap}
+        setIsVisibleMap={setIsVisibleMap}
+        setLocationRestaurant={setLocationRestaurant}
+        toastRef={toastRef}
+      />
     </ScrollView>
+  );
+}
+
+function ImageRestaurant(props) {
+  const { imageRestaurant } = props;
+  return (
+    <View style={styles.viewPhoto}>
+      {imageRestaurant ? (
+        <Image
+          source={{ uri: imageRestaurant }}
+          style={{ width: widthScreen, height: 200 }}
+        />
+      ) : (
+        <Image
+          source={require("../../../assets/img/no-image.png")}
+          style={{ width: widthScreen, height: 200 }}
+        />
+      )}
+    </View>
   );
 }
 
@@ -46,30 +88,143 @@ function UploadImage(props) {
         );
       } else {
         setImagesSelected([...imagesSelected, result.uri]);
-        console.log(imagesSelected);
       }
     }
   };
 
+  const removeImage = (image) => {
+    const arrayImages = imagesSelected;
+    Alert.alert(
+      "Eliminar Imagen",
+      "¿Estás seguro de que quieres elminar la imagen?",
+      [
+        {
+          text: "Cancel",
+          style: "cancel",
+        },
+        {
+          text: "Eliminar",
+          onPress: () =>
+            setImagesSelected(
+              arrayImages.filter((imageUrl) => imageUrl !== image)
+            ),
+        },
+      ]
+    );
+  };
+
   return (
     <View style={styles.viewImage}>
-      <Icon
-        type="material-community"
-        name="camera"
-        color="#7a7a7a"
-        containerStyle={styles.containerIcon}
-        onPress={imageSelect}
+      {imagesSelected.length < 5 && (
+        <Icon
+          type="material-community"
+          name="camera"
+          color="#7a7a7a"
+          containerStyle={styles.containerIcon}
+          onPress={imageSelect}
+        />
+      )}
+
+      {imagesSelected.map((imageRestaurant, index) => (
+        <Avatar
+          key={index}
+          onPress={() => removeImage(imageRestaurant)}
+          style={styles.miniatureStyle}
+          source={{ uri: imageRestaurant }}
+        />
+      ))}
+    </View>
+  );
+}
+
+function FormAdd(props) {
+  const {
+    setRestaurantName,
+    setRestaurantAddress,
+    setRestaurantDescription,
+    setIsVisibleMap,
+    locationRestaurant,
+  } = props;
+  return (
+    <View styles={styles.viewForm}>
+      <Input
+        placeholder="Nombre del restaurante"
+        containerStyle={styles.input}
+        onChange={(e) => setRestaurantName(e.nativeEvent.text)}
       />
-      <Avatar
-        onPress={() => console.log("Eliminando imagen")}
-        style={styles.miniatureStyle}
-        // source={{url miniatura del restaurante}
+      <Input
+        placeholder="Dirección"
+        containerStyle={styles.input}
+        rightIcon={{
+          type: "material-community",
+          name: "google-maps",
+          color: locationRestaurant ? "#00a680" : "#c2c2c2",
+          onPress: () => setIsVisibleMap(true),
+        }}
+        onChange={(e) => setRestaurantAddress(e.nativeEvent.text)}
+      />
+      <Input
+        placeholder="Descripción del restaurante"
+        multiline={true}
+        inputContainerStyle={styles.textArea}
+        onChange={(e) => setRestaurantDescription(e.nativeEvent.text)}
       />
     </View>
   );
 }
 
+function Map(props) {
+  const {
+    isVisibleMap,
+    setIsVisibleMap,
+    setLocationRestaurant,
+    toastRef,
+  } = props;
+  const [location, setLocation] = useState(null);
+
+  return (
+    <Modal isVisible={isVisibleMap} setIsVisible={setIsVisibleMap}>
+      <View>
+        {location && (
+          <MapView
+            style={styles.mapStyle}
+            initialRegion={location}
+            showUserLocation={true}
+            onRegionChange={(region) => setLocation(region)}
+          >
+            <MapView.Marker
+              coordinate={{
+                latitude: location.latitude,
+                longitude: location.longitude,
+              }}
+              draggable
+            />
+          </MapView>
+        )}
+        <View style={styles.viewMapBtn}>
+          <Button
+            title="Guardar ubicación"
+            onPress={() => console.log("Ubicación guardada")}
+            containerStyle={styles.viewMapBtnContainerSave}
+            buttonStyle={styles.viewMapBtnSave}
+          />
+          <Button
+            title="Cancelar ubicación"
+            onPress={() => setIsVisibleMap(false)}
+            containerStyle={styles.viewMapBtnContainerCancel}
+            buttonStyle={styles.viewMapBtnCancel}
+          />
+        </View>
+      </View>
+    </Modal>
+  );
+}
 const styles = StyleSheet.create({
+  viewPhoto: {
+    alignItems: "center",
+    height: 200,
+    marginBottom: 20,
+  },
   viewImage: {
     flexDirection: "row",
     marginLeft: 20,
@@ -88,5 +243,39 @@ const styles = StyleSheet.create({
     width: 70,
     height: 70,
     marginRight: 10,
+  },
+  viewForm: {
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  input: {
+    marginBottom: 10,
+  },
+  textArea: {
+    height: 100,
+    width: "100%",
+    padding: 0,
+    margin: 0,
+  },
+  mapStyle: {
+    width: "100%",
+    height: 150,
+  },
+  viewMapBtn: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: 10,
+  },
+  viewMapBtnContainerSave: {
+    paddingRight: 5,
+  },
+  viewMapBtnSave: {
+    backgroundColor: "#00a680",
+  },
+  viewMapBtnContainerCancel: {
+    paddingLeft: 5,
+  },
+  viewMapBtnCancel: {
+    backgroundColor: "#a60d0d",
   },
 });
